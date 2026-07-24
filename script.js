195bf9a7896e35d194bc98a1100f95182480389c
shaxrizodasamatova-debug <shaxrizodasamatova@icloud.com>
2026-07-24 10:13:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
-const button = document.getElementById("openDoor");
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+}
 
-button.addEventListener("click", () => {
-    alert("Taklifnoma tez orada ochiladi ❤️");
-});
+body{
+  background:linear-gradient(180deg,#f8f6f1,#efe6d7);
+  font-family:Georgia,serif;
+  min-height:100vh;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
+
+.cover{
+  text-align:center;
+}
+
+.cover h1{
+  color:#1d4f91;
+  font-size:60px;
+  font-weight:400;
+}
+
+.and{
+  font-size:40px;
+  color:#caa44f;
+  margin:15px 0;
+}
+
+.date{
+  margin-top:15px;
+  font-size:22px;
+  color:#666;
+}
+
+#openBtn{
+  margin-top:40px;
+  padding:18px 45px;
+  border:none;
+  border-radius:50px;
+  background:#2d6fb2;
+  color:white;
+  font-size:20px;
+  cursor:pointer;
+  transition:.3s;
+}
+
+#openBtn:hover{
+  transform:scale(1.05);
+}
